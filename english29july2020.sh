499d9c4448bb606cd3aeef138a57e2ca2d88d83a
Grecia Hernandez <greciavgh@gmail.com>
2020-08-04 22:17:45 -0300

    segundo cambio

---
+++ english29july2020.sh
@@ -0,0 +1,68 @@
+my morning routine:
+    #900am.  i wake up , make the bed, change my baby´s diaper.
+    #930am.  brush my teeth, wash my face, fix my hair
+    #1000am. make the breakfast and eat it.
+    #1045am. make an excersice routine, depends the day it's a 45 or 60 minutes of hard work.
+    #1100am. take a shower, and make an exfoliaton in all my body with a mix of cooffe, oats and suggar.
+    #1200am. prepare the lunch, usually is: a portion of protein and a lot of vegetables, as well for my daughter but a less portion.
+    #100pm. sometimes i take a nap with Dana for almost two hours.
+    
+    Breakfast: i have a little menu to try to organize my breakfast, i always mix eggs, bread, butter, onios, tomatoes and other flavors, but im try to make it healthy.
+    the coffee is a good companion of those dishes.
+
+    Room: describe your Room ( 
+     we have, I have, there are o there is
+1; there is a double bed, and it has a white sheet with gray
+
+2; we have 4 pillows
+3; the bed has a duvet and a blue blanket 
+4; the bed has a gray headboard
+5; in the room there is a baby cradle
+6; in the room there is a white drawer 9 drawers
+7; in the room there is a window has grey curtains standard size
+8; in the ceiling there is a white lamp has 2 light bulbs
+9; indoors indoors there is drawer 
+10; there is a closet and indoors has a drawer 4 drawers, and also has clothes rack
+11; it is a room with 4 white walls
+
+
+
+        TENEMOS, TENGO, HAY
+1; HAY UNA CAMA MATRIMONIAL, QUE TIENE UNA SABANA BLANCA CON GRIS
+2; TENEMOS 4 ALMOHADAS
+3; LA CAMA TIENE UN EDREDON Y MANTA AZUL
+4; LA CAMA TIENE RESPALDAR GRIS
+5; TENEMOS LA CUNA DE LA BEBE
+6; HAY UN GAVETERO DE 9 GAVETAS QUE ES BLANCO
+7; HAY UNA VENTANA QUE TIENE UNAS CORTINAS GRISES TAMAÑO ESTANDAR
+8; EN EL TECHO HAY UNA LAMPARA REDONDA BLANCA DE 2 BOMBILLOS
+9; HAY UN CLOSET DE DOS PUERTAS, ADENTRO TIENE UN GAVETERO DE 4 GAVETAS Y DOS PERCHEROS
+10; LA HABITACION TIENE UN CALEFACTOR BLANCO
+11; ES UNA HABITACION DE 4 PAREDES Y QUE SON DE COLOR BLANCO
+
+    )
+    Bathroom: describe your bathroom
+
+1; MI BAÑO ES PEQUEÑO
+2;    CUENTA CON UNA BAÑERA DE TAMAÑO ESTANDAR COLOR BLANCO
+  3;  HAY UNA REGADERA DE METAL
+    TENEMOS UNA CORTINA BLANCA Y NEGRA
+    HAY UN LAVAMANOS QUE CUENTA CON UN PEQUEÑO GAVETERO
+    HAY UN ESPEJO DE COLOR NEGRO.
+    HAY UN BIDET Y UNA POCETA DE COLOR BLANCO
+    HAY UNA LAMPARA DE VIDRIO 
+
+
+
+
+    900am. ME LEVANTO, HAGO LA CAMA, LE CAMBIO EL PAÑAL A MI BEBE
+    930am. ME CEPILLO LOS DIENTES, LAVO MI CARA, ARREGLO MI CABELLO
+    1000am. HAGO EL DESAYUNO Y COMO 
+    1045am. HAGO UNA RUTINA DE EJERCICIOS, DEPENDE DEL DIA QUE SEA 45 O 60 MINNUTOS DE TRABAJO DURO
+    1100am. TOMO UN BAÑO, Y ME HAGO UNA EXFOLIACION EN TODO EL CUERPO CON CAFE, AVENA Y AZUCAR.
+    1200am. PREPARO EL ALMUERZO, POR LO GENERAL ES: UNA PORCION DE PROTEINA Y MUCHOS VEGETALES, TAMBIEN PARA MI HIJA PERO UNA POCION MENOR
+    1OOpm. A VECES TOMO UNA SIESTA CON DANA DURANTE DOS HORAS.
+
+    DESAYUNO: TENGO UN PEQUEÑO MENU PARA TRATAR DE ORGANIZARME. SIEMPRE MEZCLO HUEVOS, PAN, MANTEQUILLA, TOMATE Y OTROS SABORES, PERO SIEMPRE TRATO DE HACERLO SALUDABLE
+    EL CAFE ES UNA BUENA COMPAÑIA PARA ESO PLATOS. 
+
